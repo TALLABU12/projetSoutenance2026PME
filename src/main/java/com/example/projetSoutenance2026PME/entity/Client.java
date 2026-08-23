d4dc5d266fc1ed931354ddfa0d6489abccae06be
babacartall <tallaboubacar165@gmail.com>
2026-08-23 20:45:29 +0000

    feat(client): entité Client avec callbacks de cycle de vie

--- src/main/java/com/example/projetSoutenance2026PME/entity/Client.java
+++ src/main/java/com/example/projetSoutenance2026PME/entity/Client.java
@@ -1,7 +1,7 @@
 package com.example.projetSoutenance2026PME.entity;
 import jakarta.persistence.*;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "clients")
@@ -16,10 +16,85 @@ public class Client {
     private String nom;
     @Column(nullable=false)
     private String telephone;
-    @Column(unique = true, nullable=false)
     private String email;
-    @Column(nullable=false)
     private String adresse;
-    private LocalDate dateCreation;
-    private Boolean actif;
+    @Column(nullable=false)
+    private LocalDateTime dateCreation;
+    private LocalDateTime dateModification;
+
+    @Column(nullable=false)
+    private boolean actif;
+
+    public Client() {
+    }
+
+    public Client(String code, String nom, String telephone, String email, String adresse) {
+        this.code = code;
+        this.nom = nom;
+        this.telephone = telephone;
+        this.email = email;
+        this.adresse = adresse;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.dateCreation = LocalDateTime.now();
+        this.actif = true;
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.dateModification = LocalDateTime.now();
+    }
+
+    public LocalDateTime getDateModification() {
+        return dateModification;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public LocalDateTime getDateCreation() {
+        return dateCreation;
+    }
+
+    public boolean isActif() {
+        return actif;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
 }
